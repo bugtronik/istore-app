b5de75f0ba5ac5e8691e67a0c30cf7d0760ba3bc
bugtronik <bugtronik@gmail.com>
2025-03-23 20:12:28 +0100

    configuration de la barre de navigation

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,21 +1,22 @@
 import React from 'react';
 import { Tabs } from "expo-router";
 import { Ionicons } from '@expo/vector-icons';
+import { TabBar } from '@/components/TabBar';
 
 export default function TabLayout() {
   return (
-    <Tabs screenOptions={{headerShown: false}}>
+    <Tabs tabBar={props => <TabBar {...props} />} screenOptions={{headerShown: false}}>
       <Tabs.Screen name='index' options={{
-        title: 'Home',
-        tabBarIcon: ({color}) => (
-          <Ionicons name='home-outline' size={22} color={color} />
-        )
+        title: 'Accueil',
+        //tabBarIcon: ({color}) => (
+        //  <Ionicons name='home-outline' size={22} color={color} />
+        //)
       }} />
       <Tabs.Screen name='explore' options={{
-        title: 'Explore',
-        tabBarIcon: ({color}) => (
-          <Ionicons name='search-outline' size={22} color={color} />
-        )
+        title: 'Explorer',
+        //tabBarIcon: ({color}) => (
+        //  <Ionicons name='search-outline' size={22} color={color} />
+        //)
       }} />
       <Tabs.Screen name='notifications' options={{
         title: 'Notification',
@@ -24,17 +25,17 @@ export default function TabLayout() {
         )
       }} />
       <Tabs.Screen name='cart' options={{
-        title: 'Cart',
+        title: 'Panier',
         tabBarBadge: 3,
-        tabBarIcon: ({color}) => (
-          <Ionicons name='cart-outline' size={22} color={color} />
-        )
+        //tabBarIcon: ({color}) => (
+        //  <Ionicons name='cart-outline' size={22} color={color} />
+        //)
       }} />
       <Tabs.Screen name='profile' options={{
         title: 'Profile',
-        tabBarIcon: ({color}) => (
-          <Ionicons name='person-outline' size={22} color={color} />
-        )
+        //tabBarIcon: ({color}) => (
+        //  <Ionicons name='person-outline' size={22} color={color} />
+        //)
       }} />
     </Tabs>
   );
